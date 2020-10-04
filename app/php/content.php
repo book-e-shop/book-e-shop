74ec39f2ce9ba0e8a75f2c29965573ca4abc5029
AndreyNaidenko <39535131+AndreyNaidenko@users.noreply.github.com>
2020-10-04 19:14:28 +0500

    Сделан грубый вариант формы входа, добавил книги
    Не получилось сделать ссылку от Все книги к content.php

---
+++ app/php/content.php
@@ -0,0 +1,166 @@
+<?php
+include getcwd() . "header.php";
+?>
+
+<div class='container body-content'>
+    <div class="row">
+        <div class='row'>
+            <div class="col">
+                <figure class="sign">
+                    <a href="info_book.html"><img src="../assets/images/10.jpeg" width="180px" height="256px"></a>
+                    <figcaption>Агата Кристи. Десять негритят
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/alice.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Льюис Кэрролл. Алиса в стране чудес. Алиса в зазеркалье
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/capdaut.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>А. С. Пушкин. Капитанская дочка
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/dads.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>И. С. Тургенев. Отцы и дети
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/dead.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Н. В. Гоголь. Мертвые души
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+
+        <div class='row'>
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/friends.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Эрих Мария Ремарк. Три товарища
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/kill.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Агата Кристи. Убийство в восточном экспрессе
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/loser.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>С. Я. Маршак. Вот такой рассеянный
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/master.jpeg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>М. В. Булгаков. Мастер и маргарита
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/mathhist.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Энн Руни. История математики
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+
+        <div class='row'>
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/sherlock.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Артур Конан Дойл. Шерлок Холмс
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/ship.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Джанни Родари. Приключения Чиполино
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/top.jpeg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Никколо Макиавелли. Государь
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/unknown.jpg" width="180px" height="256px"></a>
+                    </p>
+                    <figcaption>Н. Н. Носов. Незнайка на Луне
+                    </figcaption>
+                </figure>
+            </div>
+
+            <div class="col">
+                <figure class="sign">
+                    <p><a href="#"><img src="../assets/images/war.jpg" width="180px" height="256px"></a></p>
+                    <figcaption>Л. Н. Толстой. Война и мир
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<p></p>
+<nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+        <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Назад</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#">Вперед</a>
+        </li>
+    </ul>
+</nav>
+
+<?php
+include getcwd() . "footer.php";
+?>
--- app/php/header.php
+++ app/php/header.php
@@ -5,14 +5,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="../assets/css/books.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
 
 
 </head>
@@ -20,7 +27,6 @@
 <body>
 
     <nav class="navbar fixed-top bg-light  menu" id="menu">
-
 
         <div class="col-md-2">
             <a href="/">
@@ -43,12 +49,65 @@
 
         </div>
         <div class="col-md-1">
-            <a href="php/lk_main.php">
+            <a href="" data-toggle="modal" data-target="#modalLoginForm">
                 <h2><i class="fas fa-sign-in-alt"></i></h2>
 
             </a>
         </div>
     </nav>
+
+    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Авторизация/Регистрация</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" data-toggle="tab" href="#Login" role='tab'>Авторизация</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-toggle="tab" href="#Registration" role='tab'>Регистрация</a>
+                        </li>
+                    </ul>
+
+                    <div class='tab-content'>
+                        <div class="tab-pane fade show active" id="Login">
+                            <input type="email" id="defaultForm-email" class="form-control validate"
+                                placeholder="Почта">
+                            <p></p>
+                            <input type="password" id="defaultForm-pass" class="form-control validate"
+                                placeholder="Пароль">
+                            <p></p>
+                            <button type="button" class="btn btn-primary" data-toggle="button">Войти</button>
+                            <p></p>
+                        </div>
+
+                        <div class="tab-pane fade" id="Registration">
+                            <input type="text" id="fullName" placeholder="ФИО" class="form-control"> 
+                            <p></p>
+                            <input type="email" id="defaultForm-email" class="form-control validate"
+                                placeholder="Почта">
+                            <p></p>
+                            <input type="password" id="defaultForm-pass" class="form-control validate"
+                                placeholder="Пароль">
+                            <p></p>
+                            <button type="button" class="btn btn-primary"
+                                data-toggle="button">Зарегистрироваться</button>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 
     <nav id="sidebar">
 
@@ -58,7 +117,7 @@
 
         <ul class="list-unstyled components">
             <p>Категории книг</p>
-            <li class="active"><a href="books.html">Все книги</a></li>
+            <li class="active"><a href="#">Все книги</a></li>
             <li><a href="#">Классическая литература</a></li>
             <li><a href="#">Детективы</a></li>
             <li><a href="#">Для детей</a></li>
