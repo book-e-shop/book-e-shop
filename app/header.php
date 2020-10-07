3164876fa3e56f65ff012b8c3c6b250e8829e930
AndreyNaidenko <39535131+AndreyNaidenko@users.noreply.github.com>
2020-10-07 21:45:09 +0500

    Добавил выпадающий список и возможность выхода

--- app/header.php
+++ app/header.php
@@ -121,15 +121,20 @@ if (isset($data['do_login'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="../assets/css/books.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
     </script>
 
 
@@ -162,21 +167,29 @@ if (isset($data['do_login'])) {
         <div class="col-md-2">
             <?php if (isset($_SESSION['logged_user'])) : ?>
 
-                <a href="/lk_main.php">
-                    <h2><?php echo $_SESSION['logged_user']->name . ' ' . $_SESSION['logged_user']->surname; ?></h2>
-                </a>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <?php echo $_SESSION['logged_user']->name . ' ' . $_SESSION['logged_user']->surname; ?>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="/lk_main.php">Личный кабинет</a>
+                    <a class="dropdown-item" href="/logout.php">Выйти</a>
+                </div>
+            </div>
 
             <?php else : ?>
 
-                <a href="" data-toggle="modal" data-target="#modalLoginForm">
-                    <h2><i class="fas fa-sign-in-alt"></i></h2>
-                </a>
+            <a href="" data-toggle="modal" data-target="#modalLoginForm">
+                <h2><i class="fas fa-sign-in-alt"></i></h2>
+            </a>
 
             <?php endif; ?>
         </div>
     </nav>
 
-    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header text-center">
@@ -199,30 +212,52 @@ if (isset($data['do_login'])) {
                     <div class='tab-content'>
                         <div class="tab-pane fade show active" id="Login">
                             <form action='/' method='post'>
-                                <input type="text" id="login" name='login' placeholder="Логин" class="form-control" required>
-                                <p></p>
-                                <input type="password" name='password' id="defaultForm-pass" class="form-control validate" placeholder="Пароль" required>
-                                <p></p>
-                                <button type="submit" name='do_login' class="btn btn-primary" data-toggle="button">Войти</button>
+                                <div class="form-group">
+                                    <label for="login">Логин</label>
+                                    <input type="text" id="login" name='login' class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Пароль</label>
+                                    <input type="password" id="password" name='password' class="form-control validate"
+                                        required>
+                                </div>
+                                <button type="submit" name='do_login' class="btn btn-primary"
+                                    data-toggle="button">Войти</button>
                                 <p></p>
                             </form>
                         </div>
 
                         <div class="tab-pane fade" id="Registration">
                             <form action='/' method='post'>
-                                <input type="text" id="surname" name='surname' placeholder="Фамилия" class="form-control" required>
-                                <p></p>
-                                <input type="text" id="name" name='name' placeholder="Имя" class="form-control" required>
-                                <p></p>
-                                <input type="email" id="email" name='email' class="form-control validate" placeholder="Почта" required>
-                                <p></p>
-                                <input type="text" id="login" name='login' placeholder="Логин" class="form-control" required>
-                                <p></p>
-                                <input type="password" id="password" name='password' class="form-control validate" placeholder="Пароль" required>
-                                <p></p>
-                                <input type="password" id="password_2" name='password_2' class="form-control validate" placeholder="Повторите пароль" required>
-                                <p></p>
-                                <button type="submit" name='do_signup' class="btn btn-primary" data-toggle="button">Зарегистрироваться</button>
+                                <div class="form-group">
+                                    <label for="surname">Фамилия</label>
+                                    <input type="text" id="surname" name='surname' class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">Имя</label>
+                                    <input type="text" id="name" name='name' class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Почта</label>
+                                    <input type="email" id="email" name='email' class="form-control validate" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="login">Логин</label>
+                                    <input type="text" id="login" name='login' class="form-control" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Пароль</label>
+                                    <input type="password" id="password" name='password' class="form-control validate"
+                                        required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password_2">Повторите пароль</label>
+                                    <input type="password" id="password_2" name='password_2'
+                                        class="form-control validate" required>
+                                </div>
+
+                                <button type="submit" name='do_signup' class="btn btn-primary"
+                                    data-toggle="button">Зарегистрироваться</button>
                                 <p></p>
                             </form>
                         </div>
