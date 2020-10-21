ea509f3d392101bbb26ab99a35324a757aa01369
AndreyNaidenko <39535131+AndreyNaidenko@users.noreply.github.com>
2020-10-21 20:03:34 +0400

    Добавил функцию парсинга тега а

---
+++ app/parser.php
@@ -0,0 +1,20 @@
+<?php
+
+function extractLinks($html)
+{
+    $htmlDom = new DOMDocument;
+    $extractedLinks = array();
+
+    @$htmlDom->loadHTML($html);
+    $links = $htmlDom->getElementsByTagName('a');
+
+    foreach ($links as $link) {
+
+        $linkText = $link->nodeValue;
+        $linkHref = $link->getAttribute('href');
+
+        $extractedLinks[] = array($linkHref => $linkText);        
+    }
+
+    return $extractedLinks;
+}
