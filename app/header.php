205b60adf36d0f158d78725ece5e7126e5528c01
Denis Medvedev <37630193+MedvedevDenis@users.noreply.github.com>
2020-12-30 23:25:21 +0400

    Update header.php

--- app/header.php
+++ app/header.php
@@ -61,7 +61,7 @@
 
             <?php else : ?>
 
-                <a href="" data-toggle="modal" data-target="#modalLoginForm">
+                <a href="" data-toggle="modal" data-target="#captchaModal">
                     <h2 id='signIn'><i class="fas fa-sign-in-alt"></i></h2>
                 </a>
 
